f9b57b3655844b7c7fe8a3e1da7e890df0f1e45b
Kirsten Keels <keelsk@gmail.com>
2020-04-14 19:38:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_status.rb
+++ db/migrate/002_add_active_status.rb
@@ -1,2 +1,8 @@
 class AddActiveStatus < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean
+    Student.all.each do |student|
+      product.update_attributes!(:active => 'false')
+    end
+  end
 end
